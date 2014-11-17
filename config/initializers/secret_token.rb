5abfe2862691f28a0e395014c7b0a44afebe5deb
vivekmshah <vivek.shah@falkonry.com>
2014-11-17 09:29:26 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Moochify::Application.config.secret_key_base = '52c30f6aa45ff6b57cd198e65b21efae328734112201e3501924bddb60524c59a00a262aaed4f336f1b900e35e97f823f61be00a70f8a4d212cd8898aae020dc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Moochify::Application.config.session_store :cookie_store, key: '_moochify_session'
