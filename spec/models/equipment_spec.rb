b1924701adec44d37f878a5c98d03b28d58c234c
paulgarbett <paulcgarbett@gmail.com>
2014-11-18 18:52:40 -0800

    equipment tests

--- spec/models/equipment_spec.rb
+++ spec/models/equipment_spec.rb
@@ -3,5 +3,21 @@ require 'rails_helper'
 require 'shoulda/matchers'
 
 describe Equipment do
+
+it { should validate_length_of(:name).is_at_least(1) }
+it { should validate_length_of(:name).is_at_most(45) }
+
+it { should validate_numericality_of(:current_daily_cost) }
+
+it {should validate_length_of (:description).is_at_most(255)}
+
+it {should validate_length_of (:availability).is_at_most(100)}
+
+it { should validate_length_of(:condition).is_at_least(1) }
+it { should validate_length_of(:condition).is_at_most(100) }
+
+it { should belong_to(:user) }
+
+it { should have_many(:orders) }
 	
 end
