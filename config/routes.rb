32de385b327d8fcc1cdc0130d522c799e3c3e485
vivekmshah <vivek.shah@falkonry.com>
2014-11-19 16:42:46 -0800

    Homepage root, logout in header

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Moochify::Application.routes.draw do
 
+  root 'home#show', as: :home
+
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
-  get 'logout', to: 'sessions#destroy', as: 'logout'
+  delete 'logout', to: 'sessions#destroy', as: 'logout'
 
   resources :users
   resources :equipment
