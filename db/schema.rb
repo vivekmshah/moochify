8e9854b884c871b14cb2486e55b1420140d1508f
vivekmshah <vivek.shah@falkonry.com>
2014-11-29 15:13:13 -0800

    phone bigint format

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141129213722) do
+ActiveRecord::Schema.define(version: 20141129222855) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,7 +53,7 @@ ActiveRecord::Schema.define(version: 20141129213722) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
-    t.integer  "phone"
+    t.integer  "phone",               limit: 8
     t.string   "address_1"
     t.string   "address_2"
     t.string   "city"
