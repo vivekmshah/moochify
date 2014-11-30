076030c68aa417487a27cf7fc9cf2a97a20e7905
paulgarbett <paulcgarbett@gmail.com>
2014-11-29 17:54:39 -0800

    order view pages

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,12 +6,14 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
+    @user = User.find(@order.user_id)
+    @equipment = Equipment.find(@order.equipment_id)
   end
 
   def create
-    order = current_user.orders.new(params.permit(:start_date, :end_date, :equipment_id))
+    order = current_user.orders.new(params.permit(:start_date, :end_date, :daily_cost, :equipment_id))
     order.save
-    redirect_to equipment_index_path  
+    redirect_to order_path(order)  
   end
 
 end
