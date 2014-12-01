049042c32c1e7231ff8fb06556e005e3a053dd15
JamesPF <jamesfelz@gmail.com>
2014-12-01 12:11:43 -0800

    styling changes and heroku related changes

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,5 +1,6 @@
 class Result < ActiveRecord::Base
   
+  #IMPORTANT!!!  I think the code below may be useless!!
   def self.search(search)
   	search_condition = "%" + search + "%"
   	find(:all, :conditions => ['title LIKE ? OR description LIKE?', search_condition, search_condition])
