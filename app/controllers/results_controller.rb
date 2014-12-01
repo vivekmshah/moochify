ef580e9389ce9414af62aa7d33e34103ed72b7f9
vivekmshah <vivek.shah@falkonry.com>
2014-12-01 14:59:02 -0800

    google maps render after geocode

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -7,28 +7,15 @@ class ResultsController < ApplicationController
     @zip = params[:zip]
 
     @results = Equipment.all
-    @results = @results.where("name LIKE ?", "%#{params[:name]}%") if !params[:name].blank?
+    @results = @results.where("lower(name) LIKE ?", "%#{params[:name].downcase}%") if !params[:name].blank?
     @results = @results.joins(:user)
     @results = @results.where(:users => {:zip => params[:zip]}) if !params[:zip].blank?
 
-    # @results = @results.where(zip: params[:zip]) if !params[:zip].blank?
-
-    # @locations = User.find_by_sql([
-    #   'SELECT users.id, users.longitude, users.latitude ' +
-    #   'FROM users ' +
-    #   'INNER JOIN equipment ' +
-    #   'ON users.id = equipment.user_id ' +
-    #   # 'WHERE users.zip = ? ' +
-    #   # 'AND equipment.name LIKE ?',
-    #   'WHERE equipment.name LIKE ?',
-    #   # "#{params[:zip]}",
-    #   "%#{params[:name]}%"
-    # ])
-
     @locations = User.all
+    @locations = @locations.select(:longitude, :latitude)
     @locations = @locations.where(zip: params[:zip]) if !params[:zip].blank?
     @locations = @locations.joins(:equipment) if !params[:name].blank?
-    @locations = @locations.where("equipment.name LIKE ?", "%#{params[:name]}%") if !params[:name].blank?
+    @locations = @locations.where("lower(equipment.name) LIKE ?", "%#{params[:name].downcase}%") if !params[:name].blank?
 
   end
 
@@ -40,10 +27,3 @@ class ResultsController < ApplicationController
   end
 
 end
-
-# Post.joins(:group => :memberships).where(:memberships => {:user_id => 1})
-# SELECT "posts".* FROM "posts" INNER JOIN "groups" ON "groups"."id" = "posts"."group_id" INNER JOIN "memberships" ON "memberships"."group_id" = "groups"."id" WHERE ("memberships"."user_id" = 1)
-
-# Speaker.all(:conditions => ["name like ?", "%yson%"])
-
-    # @locations = @locations.where("equipment.name LIKE ?", "%#{params[:name]}%")
