0aafe4579c284de4dee8482f200bcdff46016151
JamesPF <jamesfelz@gmail.com>
2014-12-01 15:27:53 -0800

    delete some uneeded code from result.rb that was left over from when i tried figuring out the search

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,9 +1,3 @@
 class Result < ActiveRecord::Base
-  
-  #IMPORTANT!!!  I think the code below may be useless!!
-  def self.search(search)
-  	search_condition = "%" + search + "%"
-  	find(:all, :conditions => ['title LIKE ? OR description LIKE?', search_condition, search_condition])
-  end
-
+ 
 end
